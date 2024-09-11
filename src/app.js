5d1cdf6f16ae284440a4f8abd89d4c7497c0a4fe
awaisarshad819 <awaisarshad819@gmail.com>
2024-09-11 18:07:51 +0500

    User routes and user controller basic setup

--- src/app.js
+++ src/app.js
@@ -9,4 +9,11 @@ app.use(express.json({ limit: "16KB" }));
 app.use(express.urlencoded({ extended: true, limit: "16KB" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//Routes import
+import userRoutes from "./routes/user.routes.js";
+
+//Routes declaration
+app.use("/api/v1/users", userRoutes);
+
 export default app;
